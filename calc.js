0e957c085a1184087ace7fabd76415438381360e
Peyton Moss <pmoss94@gmail.com>
2022-09-13 12:50:49 -0600

    working calc

--- calc.js
+++ calc.js
@@ -14,8 +14,31 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
+		console.log('tokens', tokens);
+		console.log('input', input);
+
+			console.log('mathSymbol', mathSymbol);
+            console.log('num1', num1);
+            console.log('num2', num2);
+
+		if (mathSymbol === '+') {
+			console.log('answer:', num1 + num2);
+		} else if (mathSymbol === '-') {
+			console.log('answer:', num1 - num2);
+		} else if (mathSymbol === '*') {
+			console.log('answer:', num1 * num2);
+		} else if (mathSymbol === '/') {
+			console.log('answer:', num1 / num2);
+		} else if (mathSymbol === '-') {
+			console.log('answer:', num1 - num2);
+		} else if (mathSymbol === 'sqrt') {
+			console.log('answer:', Math.sqrt(num1));
+		}
+
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
