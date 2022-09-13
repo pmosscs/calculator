01652d63d3827d68a31986ad56f8a0ee098969da
Peyton Moss <pmoss94@gmail.com>
2022-09-13 13:09:24 -0600

    further study finished

--- calc.js
+++ calc.js
@@ -13,6 +13,7 @@ reader.question("What would you like to calculate?", function(input){
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
+	const num3 = Number(tokens[3]);
 
 		console.log('tokens', tokens);
 		console.log('input', input);
@@ -20,19 +21,26 @@ reader.question("What would you like to calculate?", function(input){
 			console.log('mathSymbol', mathSymbol);
             console.log('num1', num1);
             console.log('num2', num2);
+			console.log('num3', num3)
 
 		if (mathSymbol === '+') {
-			console.log('answer:', num1 + num2);
+			console.log('answer:', num1 + num2 + num3);
 		} else if (mathSymbol === '-') {
-			console.log('answer:', num1 - num2);
+			console.log('answer:', num1 - num2 - num3);
 		} else if (mathSymbol === '*') {
-			console.log('answer:', num1 * num2);
+			console.log('answer:', num1 * num2 * num3);
 		} else if (mathSymbol === '/') {
 			console.log('answer:', num1 / num2);
-		} else if (mathSymbol === '-') {
-			console.log('answer:', num1 - num2);
 		} else if (mathSymbol === 'sqrt') {
 			console.log('answer:', Math.sqrt(num1));
+		} else if (mathSymbol === 'sq') {
+			console.log('answer', num1 ** 2);
+		} else if (mathSymbol === 'cube') {
+			console.log('answer', num1 ** 3);
+		} else if (mathSymbol === 'exp') {
+			console.log('answer', num1 ** num2);
+		} else if (mathSymbol === '%') {
+			console.log('answer', num1 % num2);
 		}
 
 
@@ -42,3 +50,5 @@ reader.question("What would you like to calculate?", function(input){
 
 });
 
+
+// Find the remainder when one number is divided by another number (modulus)
